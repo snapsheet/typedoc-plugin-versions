b5e659d6d88ef4003f2feb370a8931bc9e4b9fec
David Tittle <david.tittle@snapsheet.me>
2023-07-06 16:05:24 -0500

    Committing the build for use in dependency projects until upstream changes are merged.

---
+++ dist/src/types.d.ts
@@ -0,0 +1,37 @@
+/**
+ * Type definitions for typedoc-plugin-versions
+ *
+ * @module
+ */
+/**
+ * User defined options under the key of `versions` in typedoc.json
+ */
+export interface versionsOptions {
+    /**
+     * The minor version that you would like to be marked as `stable`
+     * Defaults to the latest patch version of the version being built
+     */
+    stable?: version | 'auto';
+    /**
+     * The version that you would like to be marked as `dev`
+     * Defaults to the latest patch version of the version being built
+     */
+    dev?: version | 'auto';
+    /**
+     * A custom DOM location to render the HTML `select` dropdown corresponding to [typedoc render hooks](https://typedoc.org/api/interfaces/RendererHooks.html)
+     * Default: Injects to left of header using vanilla js - not a typedoc render hook.
+     */
+    domLocation?: validLocation | 'false';
+}
+export declare type version = `v${string}`;
+/**
+ * valid placement overrides for the select, corresponds to [typedoc render hooks](https://typedoc.org/api/interfaces/RendererHooks.html)
+ */
+export declare type validLocation = 'body.begin' | 'body.end' | 'content.begin' | 'content.end' | 'navigation.begin' | 'navigation.end';
+export declare type semanticAlias = 'stable' | 'dev';
+export declare type metadata = {
+    versions?: version[];
+    stable?: version;
+    dev?: version;
+};
+//# sourceMappingURL=types.d.ts.map
