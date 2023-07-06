3d9499530348fcfe7b094d32155b32c0c544e05c
Rohin Paikattil <paikattilrohin@gmail.com>
2023-07-06 01:36:01 -0400

    Remove dist

--- dist/src/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Type definitions for typedoc-plugin-versions
- *
- * @module
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
